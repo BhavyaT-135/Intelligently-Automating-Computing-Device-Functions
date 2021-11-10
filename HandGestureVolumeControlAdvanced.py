0685328101c793f83a36d16d9711c13ab001b1f6
Bhavya Verma <vbhavya269@gmail.com>
2021-11-10 21:04:57 +0530

    Advanced Volume Control

--- HandGestureVolumeControlAdvanced.py
+++ HandGestureVolumeControlAdvanced.py
@@ -16,7 +16,7 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
-detector = htm.handDetector(detectionCon=0.7)
+detector = htm.handDetector(detectionCon=0.7, maxHands=1)
 
 # Initializations for pycaw
 devices = AudioUtilities.GetSpeakers()
@@ -32,6 +32,8 @@ maxVol = volRange[1]
 vol = 0
 volBar = 400
 volPer = 0
+area = 0
+colorVol = (255, 0, 0)
 
 
 while True:
@@ -39,57 +41,49 @@ while True:
 
     # Find Hand
     img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
+    lmList, bbox = detector.findPosition(img, draw=True)
     if len(lmList) != 0:
 
         # Filter based on size
+        area = (bbox[2] - bbox[2])*(bbox[3] - bbox[1])//100
+        print(area)
+        if 250 < area < 1000:
 
-        # Find distance bw index and the thumb
-        
-        # Convert Volume
+            # Find distance bw index and the thumb
+            length, img, lineInfo = detector.findDistance(4, 8, img)
+            print(length)
 
-        # Reduce Resolution to make it smoother
+            # Convert Volume
+            volBar = np.interp(length, [50, 200], [400, 150])
+            volPer = np.interp(length, [50, 200], [0, 100])
 
-        # Check fingers up
+            # Reduce Resolution to make it smoother
+            smoothness = 10
+            volPer = smoothness * round(volPer/smoothness)
 
-        # Check if pinky is down set volume
-    
-        # Drawings
+            # Check fingers up
+            fingers = detector.fingersUp()
+            print(fingers)
 
-        # Frame Rate
+            # Check if pinky is down set volume
+            if not fingers[4]:
+                volume.SetMasterVolumeLevelScalar(volPer/100, None)
+                cv2.circle(img, (lineInfo[4], lineInfo[5]),
+                           15, (0, 255, 0), cv2.FILLED)
+            else:
+                colorVol = (255, 0, 0)
 
-        # print(lmList[4], lmList[8])
-
-        x1, y1 = lmList[4][1], lmList[4][2]
-        x2, y2 = lmList[8][1], lmList[8][2]
-        cx, cy = (x1+x2) // 2, (y1 + y2) // 2
-
-        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-
-        length = math.hypot(x2-x1, y2-y1)
-        print(length)
-
-        # Hand Range 50 - 300
-        # Volume Range -74 - 0
-
-        vol = np.interp(length, [50, 300], [minVol, maxVol])
-        volBar = np.interp(length, [50, 300], [400, 150])
-        volPer = np.interp(length, [50, 300], [0, 100])
-        print(int(length), vol)
-        volume.SetMasterVolumeLevel(vol, None)
-
-        if length < 50:
-            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
-
+    # Drawings
     cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
     cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
 
     cv2.putText(img, f'{int(volPer)} %', (40, 450),
                 cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
+    cVol = int(volume.GetMasterVolumeLevelScalar()*100)
+    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50),
+                cv2.FONT_HERSHEY_COMPLEX, 1, colorVol, 3)
 
+    # Frame Rate
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
