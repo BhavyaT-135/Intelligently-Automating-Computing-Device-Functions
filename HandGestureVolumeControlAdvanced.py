5f99e9fe37740074eb92df2b999ebdd63e52983c
Bhavya Verma <vbhavya269@gmail.com>
2021-11-10 23:27:13 +0530

    Fixed bugs

--- HandGestureVolumeControlAdvanced.py
+++ HandGestureVolumeControlAdvanced.py
@@ -1,29 +1,26 @@
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-from comtypes import CLSCTX_ALL
-from ctypes import cast, POINTER
 import cv2
 import time
 import numpy as np
 import HandTrackingModule as htm
 import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-###############################
+################################
 wCam, hCam = 640, 480
-###############################
+################################
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7, maxHands=1)
-
-# Initializations for pycaw
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
-
 # volume.GetMute()
 # volume.GetMasterVolumeLevel()
 volRange = volume.GetVolumeRange()
@@ -35,7 +32,6 @@ volPer = 0
 area = 0
 colorVol = (255, 0, 0)
 
-
 while True:
     success, img = cap.read()
 
@@ -45,13 +41,13 @@ while True:
     if len(lmList) != 0:
 
         # Filter based on size
-        area = (bbox[2] - bbox[2])*(bbox[3] - bbox[1])//100
-        print(area)
+        area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]) // 100
+        # print(area)
         if 250 < area < 1000:
 
-            # Find distance bw index and the thumb
+            # Find Distance between index and Thumb
             length, img, lineInfo = detector.findDistance(4, 8, img)
-            print(length)
+            # print(length)
 
             # Convert Volume
             volBar = np.interp(length, [50, 200], [400, 150])
@@ -59,37 +55,36 @@ while True:
 
             # Reduce Resolution to make it smoother
             smoothness = 10
-            volPer = smoothness * round(volPer/smoothness)
+            volPer = smoothness * round(volPer / smoothness)
 
             # Check fingers up
             fingers = detector.fingersUp()
-            print(fingers)
+            # print(fingers)
 
-            # Check if pinky is down set volume
+            # If pinky is down set volume
             if not fingers[4]:
-                volume.SetMasterVolumeLevelScalar(volPer/100, None)
+                volume.SetMasterVolumeLevelScalar(volPer / 100, None)
                 cv2.circle(img, (lineInfo[4], lineInfo[5]),
                            15, (0, 255, 0), cv2.FILLED)
+                colorVol = (0, 255, 0)
             else:
                 colorVol = (255, 0, 0)
 
     # Drawings
-    cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
-    cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
+                1, (255, 0, 0), 3)
+    cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
+    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX,
+                1, colorVol, 3)
 
-    cv2.putText(img, f'{int(volPer)} %', (40, 450),
-                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
-    cVol = int(volume.GetMasterVolumeLevelScalar()*100)
-    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50),
-                cv2.FONT_HERSHEY_COMPLEX, 1, colorVol, 3)
-
-    # Frame Rate
+    # Frame rate
     cTime = time.time()
-    fps = 1/(cTime-pTime)
+    fps = 1 / (cTime - pTime)
     pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
+                1, (255, 0, 0), 3)
 
-    cv2.putText(img, f'FPS: {int(fps)}', (40, 50),
-                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
-
-    cv2.imshow("img", img)
+    cv2.imshow("Img", img)
     cv2.waitKey(1)
